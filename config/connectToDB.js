aa379a44e23755d884623a81020a3d856c93f73c
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2022-05-07 01:12:09 +0545

    fix database connector function

--- config/connectToDB.js
+++ config/connectToDB.js
@@ -1,21 +1,19 @@
 const mongoose = require('mongoose');
 
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0,
-}
-
 const connectWithRetry = () => {
-  mongoose.connect(process.env.DB_URL, options).then(() => {
-    console.log('Connected to the MongoDB successfully.');
-  }).catch(err => {
-    console.log('MongoDB connection unsuccessful, retry after 5 seconds.');
-    setTimeout(connectWithRetry, 5000);
-  });
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+      console.log('Connected to the MongoDB successfully.');
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log(
+        'MongoDB connection unsuccessful, retry after 5 seconds.',
+        err,
+      );
+      setTimeout(connectWithRetry, 5000);
+    });
 };
 
 module.exports = connectWithRetry;
