ff84314d56eee117924973e751d39db412a09a82
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2022-05-13 12:05:52 +0545

    fix: handle imagePreview errors

--- utils/getPreviewImage.js
+++ utils/getPreviewImage.js
@@ -7,27 +7,33 @@ const metascraper = require('metascraper')([
 const axios = require('axios');
 
 const getPreviewImage = async (targetUrl) => {
-  if (targetUrl.substring(0, 4) !== 'http') {
-    targetUrl = 'http://' + targetUrl;
-  }
+  try {
+    if (targetUrl.substring(0, 4) !== 'http') {
+      targetUrl = 'http://' + targetUrl;
+    }
 
-  const response = await axios(targetUrl, {
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0',
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Accept-Encoding': 'gzip',
-      DNT: '1',
-      Connection: 'close',
-    },
-    maxContentLength: 10000000, // 10 mb
-    timeout: 12000, // 12 second timeout
-  });
-  const url = response.request.res.responseUrl;
-  const html = response.data;
-  const metadata = await metascraper({ html, url });
-  return metadata.image || metadata.logo;
+    const response = await axios(targetUrl, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip',
+        DNT: '1',
+        Connection: 'close',
+      },
+      maxContentLength: 10000000, // 10 mb
+      timeout: 12000, // 12 second timeout
+    });
+    const url = response.request.res.responseUrl;
+    const html = response.data;
+    const metadata = await metascraper({ html, url });
+    return metadata.image || metadata.logo;
+  } catch (err) {
+    // sentry logger
+    return null;
+  }
 };
 
 module.exports = getPreviewImage;
